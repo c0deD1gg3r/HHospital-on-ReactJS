30db79472e71967dd880433f697c9a661bcb6857
c0deD1gg3r <122603101+c0deD1gg3r@users.noreply.github.com>
2024-12-02 19:51:43 +0500

    Fixed the logo path

--- components/Pages/About/About.jsx
+++ components/Pages/About/About.jsx
@@ -1,10 +1,10 @@
 import './About.css';
-import Logo from "../../../src/img/Logo.png";
+
 const About = () => {
   return (
     <div>
       <h1>О клинике</h1>
-      <img src={Logo} alt="" />
+
     </div>
   );
 };
